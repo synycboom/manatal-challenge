c25a303c4d0cff149b84b4c3d17c2086203a3a8e
synycboom <synycboom@gmail.com>
2018-10-21 10:26:58 +0700

    Create School and Student models

--- challenge/education/models.py
+++ challenge/education/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=20)
+    max_students = models.PositiveIntegerField()
+
+    def __str__(self):
+        return '{} {}'.format(self.name, self.max_students)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    identification = models.CharField(max_length=20, editable=False, unique=True)
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {} {}'.format(
+            self.identification,
+            self.first_name,
+            self.last_name,
+        )
